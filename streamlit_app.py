05a9416c4697455345273f1c2faa036a0488f77b
arorasudhir <sudhir.arora@gmail.com>
2023-08-11 11:21:02 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,8 @@ streamlit.title('Learning Streamlit')
 streamlit.header('Requirements:')
 streamlit.text('Snowflake')
 streamlit.text('Github')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
