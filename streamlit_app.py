badee0aee859a01c2c6650115f690b466021a92b
arorasudhir <sudhir.arora@gmail.com>
2023-08-14 15:20:03 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,4 +15,4 @@ streamlit.dataframe(fruits_to_show)
 
 streamlit.header('API')
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruitvice_response)
+streamlit.text(fruityvice_response)
