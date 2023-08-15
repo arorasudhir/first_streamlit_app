6722195682b32604b6c182697281926c435af75a
arorasudhir <sudhir.arora@gmail.com>
2023-08-15 17:37:09 -0500

    Update dbt_run_results.py

--- dbt_run_results.py
+++ dbt_run_results.py
@@ -15,15 +15,16 @@ def get_run_results():
         query = "SELECT * from DBT_RESULTS t;"
         cur.execute(query)
         return cur.fetchall()
-        # df = pd.DataFrame(dat, columns=[col[0] for col in cur.description])
-        # return df
+        df = pd.DataFrame(cur.fetchall())
+        return df
 
 streamlit.title('DBT RUN RESULTS')
 run_results = get_run_results()
-df = pd.dataframe(run_results)
+streamlit.dataframe(run_results)
+#df = pd.dataframe(run_results)
 
-streamlit.metric(label="Models", value=df['2'].nunique(), delta="1.2 °F")
+#streamlit.metric(label="Models", value=df['2'].nunique(), delta="1.2 °F")
 
-streamlit.dataframe(df)
+#streamlit.dataframe(df)
 
 
