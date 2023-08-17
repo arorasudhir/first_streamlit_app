b765a4a6acb58d00a33f232a668b88f6b7def4d5
arorasudhir <sudhir.arora@gmail.com>
2023-08-17 12:15:22 -0500

    Update streamlit_map.py

--- streamlit_map.py
+++ streamlit_map.py
@@ -8,7 +8,7 @@ streamlit.set_page_config(layout="wide")
 
 data = pandas.read_csv('users_location.csv')
 user_count = streamlit.slider('User Count', 0, 10000000)
-data = data[int(data['USERS']) == user_count]
+data = data[data['USERS'] <= user_count]
 
 streamlit.header('Where our Customers come from?')
 streamlit.map(data, zoom=7.5)
