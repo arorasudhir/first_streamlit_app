4c8e946100d81a925f96a4ee379acebdd2b16927
arorasudhir <sudhir.arora@gmail.com>
2023-08-21 13:59:46 -0500

    Update streamlit_map.py

--- streamlit_map.py
+++ streamlit_map.py
@@ -3,9 +3,5 @@ import streamlit as st
 
 data = pandas.read_csv('users_location.csv')
 
-user_count = st.slider('User Count', 0, 1000)
-
-data = data[data['USERS'] > user_count]
-
 st.header('Where our Users come from?')
 st.map(data, zoom=1)
